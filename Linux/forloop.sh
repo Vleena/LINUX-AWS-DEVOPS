521fbb44fbd128c6ff0e921b92dd5ce0727c2c7f
Vleena <vleena@gmail.com>
2025-07-15 22:26:33 +0530

    for-loop

---
+++ Linux/forloop.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+######################
+# Author: Vleena
+# Description: for loop to print a number
+# Version: V1
+
+#####################
+
+for i in {1,2,3,4,5,6,7,8,9,10};do
+	echo "number: $i"
+done
