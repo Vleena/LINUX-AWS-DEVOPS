58a5069a41b4d7524c74e4f4907c4601a0c7b147
vleena <vleena@gmail.com>
2025-08-21 09:01:16 +0000

    changed the code

--- Docker/docker_install_ubuntu.sh
+++ Docker/docker_install_ubuntu.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+####################
+#Author: Vleena
+#Description: install the docker on ubuntu
+#Version:V2
+####################
 sudo apt-get update
 sudo apt-get install docker.io -y
-
 sudo usermod -aG docker ubuntu
+sudo systemctl restart docker
 
-logout
+
