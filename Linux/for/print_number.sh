9f1410ce5bf159fcd617781c230cfce17746409e
Vleena <vleena@gmail.com>
2025-08-27 17:46:00 +0530

    updated the number print

--- Linux/for/print_number.sh
+++ Linux/for/print_number.sh
@@ -4,13 +4,14 @@
 ################################
 #Author: Vleena
 #Description: print the number 1 to 5
-#Version: V2
+#Version: V3
 
 ######################
 
 set -e
 
-for i in 1 2 3 4 5
+#for i in 1 2 3 4 5
+for i in $(seq 1 10)
 do 
 	echo $i
 done
