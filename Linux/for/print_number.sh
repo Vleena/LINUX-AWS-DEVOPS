18669b18b0e055090a33f9ebaff6afbd665e804d
Vleena <vleena@gmail.com>
2025-08-27 17:37:49 +0530

    print the numbers

---
+++ Linux/for/print_number.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+################################
+#Author: Vleena
+#Description: print the number 1 to 5
+#Version: V2
+
+######################
+
+set -e
+
+for i in 1 2 3 4 5
+do 
+	echo $i
+done
